388b3206447d58e13f74ddfd7e56d1e041d0fade
james brian <jamesgsr@gmail.com>
2020-03-26 02:22:09 +0300

    Vue frontend setup

---
+++ resources/js/components/CreateComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>Create A Smart Device</h1>
+    <form @submit.prevent="addSmartDevice">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Smart Device Title:</label>
+            <input type="text" class="form-control" v-model="smartdevice.title">
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Smart Device Body:</label>
+              <textarea class="form-control" v-model="smartdevice.body" rows="5"></textarea>
+            </div>
+          </div>
+        </div><br />
+        <div class="row">
+          <div class="col-md-6">
+            <select v-model="manufacturer">
+                  <option value='0' >Select Manufacturer</option>
+                   <option v-for="manufacturer in manufacturers" v-bind:value="{ id: manufacturer.id, text: manufacturer.title }">{{ manufacturer.title }}
+                   </option>
+               </select>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Create</button>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return {
+            post:{},
+            manufacturer: 0,
+            manufacturers:[],
+          };
+        },
+        created(){
+          let manufacturer_uri = `http://hydroiq.test/api/manufacturers`;
+          this.axios.get(manufacturer_uri).then((response2) => {
+              this.manufacturers = response2.data.data;
+              // console.log(this.categories);
+          });
+        },
+        methods: {
+          addSmartDevice(){
+            // console.log(this.post);
+            let uri = 'http://hydroiq.test/api/smartdevice/create';
+            let data = {smartdevice: this.smartdevice, manufacturer:this.manufacturer};
+            this.axios.post(uri, data).then((response) => {
+               this.$router.push({name: 'smartdevices'});
+            });
+          }
+        }
+  }
+</script>
---
+++ resources/js/components/EditComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Edit Smartdevice</h1>
+    <form @submit.prevent="updatesmartdevice">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Smartdevice Title:</label>
+            <input type="text" class="form-control" v-model="smartdevice.title" >
+          </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Smartdevice Body:</label>
+              <textarea class="form-control" v-model="smartdevice.body" rows="5"></textarea>
+            </div>
+          </div>
+        </div><br />
+        <div class="row">
+          <div class="col-md-6">
+            <select v-model="manufacturer">
+                  <option value='0' >Select Manufacturer</option>
+                   <option v-for="manufacturer in manufacturers" v-bind:value="{ id: manufacturer.id, text: manufacturer.title }">{{ manufacturer.title }}
+                   </option>
+               </select>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Update</button>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+    
+    export default {
+          // options:manufacturers
+      // cat: '1',
+      data() {
+        return {
+          smartdevice: {},
+          manufacturer: 0,
+          manufacturers:[],
+        }
+      },
+      created() {
+        let uri = `http://hydroiq.test/api/smartdevice/edit/${this.$route.params.id}`;
+        this.axios.get(uri).then((response) => {
+            this.smartdevice = response.data;
+        });
+        let uri2 = `http://hydroiq.test/api/manufacturers`;
+        this.axios.get(uri2).then((response2) => {
+            this.manufacturers = response2.data.data;
+            // console.log(this.manufacturers);
+        });
+      },
+      methods: {
+        updatesmartdevice() {
+          let uri = `http://hydroiq.test/api/smartdevice/update/${this.$route.params.id}`;
+          let data2 = {smartdevice: this.smartdevice, manufacturer:this.manufacturer};
+          this.axios.smartdevice(uri, data2).then((response) => {
+            this.$router.push({name: 'smartdevices'});
+          });
+        },
+      }
+    }
+</script>
