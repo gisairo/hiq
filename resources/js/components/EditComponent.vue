a25431f69e8ab675e950d388699bc0f75da6d252
james brian <jamesgsr@gmail.com>
2020-03-29 00:45:44 +0300

    added data validation create and edit of smart devices

--- resources/js/components/EditComponent.vue
+++ resources/js/components/EditComponent.vue
@@ -3,6 +3,10 @@
     <h1>Edit Smartdevice</h1>
     <form @submit.prevent="updatesmartdevice">
       <div class="row">
+        <section v-if="errored.serialerrormessage!=true" class="warn col-md-6 " style="margin-bottom: 20px">
+            <p class="alert alert-danger">{{ errored.serialerrormessage }}</p>
+          </section>
+          <p></p>
         <div class="col-md-6">
           <div class="form-group">
             <label>Smartdevice Serial:</label>
@@ -11,6 +15,10 @@
         </div>
         </div>
         <div class="row">
+          <section v-if="errored.descriptionerrormessage!=true" class="warn col-md-6 " style="margin-bottom: 20px">
+            <p class="alert alert-danger">{{ errored.descriptionerrormessage }}</p>
+          </section>
+          <p></p>
           <div class="col-md-6">
             <div class="form-group">
               <label>Smartdevice Description:</label>
@@ -19,9 +27,10 @@
           </div>
         </div><br />
         <div class="row">
+          
           <div class="col-md-6">
             <select v-model="manufacturer">
-                  <option value='0' >Select Manufacturer</option>
+                  <option value=false >Select Manufacturer</option>
                    <option v-for="manufacturer in manufacturers" v-bind:value="{ id: manufacturer.id, text: manufacturer.name }">{{ manufacturer.name }}
                    </option>
                </select>
@@ -44,6 +53,10 @@
           smartdevice: {},
           manufacturer: 0,
           manufacturers:[],
+          errored: {
+              serialerrormessage: true,
+              descriptionerrormessage: true,
+            },
         }
       },
       created() {
@@ -63,6 +76,23 @@
           let data2 = {smartdevice: this.smartdevice, manufacturer:this.manufacturer};
           this.axios.post(uri, data2).then((response) => {
             this.$router.push({name: 'smartdevices'});
+          }).catch(error => {
+              console.log(this.smartdevice.serial);
+              console.log(typeof(this.smartdevice.description));
+              
+              if(this.smartdevice.serial == ""){
+                this.errored.serialerrormessage = "Smart Device Serial is Required";
+                // this.errored = true;
+              }else{
+                this.errored.serialerrormessage = true;
+              }
+              if(this.smartdevice.description == ""){
+                this.errored.descriptionerrormessage = "Smart Device Description Required"
+                // this.errored = true;
+              }else{
+                this.errored.descriptionerrormessage = true;
+              }
+                
           });
         },
       }
